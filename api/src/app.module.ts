b0e15ec22b31c3a05dcd7f272628370fc3bf620e
Mesay <mesabe77@gmail.com>
2026-02-13 03:36:45 +0300

    adding health endpoint

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,11 +2,13 @@ import { LoggerModule } from 'nestjs-pino';
 import { ConfigModule } from './config/config.module';
 import { Module } from '@nestjs/common/decorators/modules/module.decorator';
 import { PrismaModule } from './prisma/prisma.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
     ConfigModule,
     PrismaModule,
+    HealthModule,
     LoggerModule.forRoot({
       pinoHttp: {
         transport:
