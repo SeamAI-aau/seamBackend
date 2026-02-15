6d46c9c207e095549b4a98d650445f7d069f0084
Mesay <mesabe77@gmail.com>
2026-02-15 03:56:52 +0300

    refactor: adjust formatting in PrismaService constructor

--- api/src/prisma/prisma.service.ts
+++ api/src/prisma/prisma.service.ts
@@ -3,10 +3,8 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-
-    constructor() {
+  constructor() {
     super();
-
   }
   async onModuleInit() {
     await this.$connect();
