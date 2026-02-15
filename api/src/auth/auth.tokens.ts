36f3fdbcee0b9aae666e25b7c180e91d33433791
Mesay <mesabe77@gmail.com>
2026-02-15 07:56:29 +0300

    Revert "Merge pull request #3 from SeamAI-aau/SCRUM-15-jwt-session-handling"
    This reverts commit a0674d06e11a13e4d1e29dd8e681d67761281bd2, reversing
    changes made to f1fcb6a3823c7beac2a0b97b210057cfd75e7b23.

--- api/src/auth/auth.tokens.ts
+++
@@ -1,2 +0,0 @@
-export const AUTH_REPOSITORY = Symbol('AUTH_REPOSITORY');
-export const JWT_SERVICE = Symbol('JWT_SERVICE');
--- api/src/auth/types/current-user.type.ts
+++
@@ -1,7 +0,0 @@
-import { Role } from '@prisma/client';
-
-export interface CurrentUserType {
-  userId: string;
-  email: string;
-  role: Role;
-}
