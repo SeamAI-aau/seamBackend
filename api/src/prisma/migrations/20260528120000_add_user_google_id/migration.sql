d73301c3c345fb949c90de3fe503e33d47c7085b
Mesay <mesabe77@gmail.com>
2026-05-30 18:03:22 +0300

    feat: add .dockerignore and .slugignore files; update package.json for migration handling

--- api/src/prisma/migrations/20260528120000_add_user_google_id/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN "googleId" TEXT;
-
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "passwordHash" DROP NOT NULL;
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_googleId_key" ON "User"("googleId");
